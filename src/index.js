d651172051a057f632a35d5c0b708312c50cec12
miamam1 <theshpeecrab@gmail.com>
2024-01-04 11:06:22 +0000

    initial commit: installed skeleton code for required files and setup npx webpack/npm run build

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+
+
+const myNuts = document.createElement("h1")
+myNuts.classList.add("'myNuts")
+myNuts.textContent = "Kill yourself. Kill yourself. Kill yourself. Kill yourself. There are bugs in your skin."
+document.body.append(myNuts)
+
+
+/* view examples of what to do, try to implement the SOLID principles talked about but probably
+not to the point where i take months to finish the list */
+
+/* before starting probably btrainstorm and check previous shit/other ppls code to see what you should be doin */
+
+/* also; for UI kinda wanna do what I did for the library project cus it looks nice and is low effort - more time to think on functionality */
+
